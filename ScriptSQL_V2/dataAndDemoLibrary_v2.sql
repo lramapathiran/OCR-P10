bd13e3002595f3cd8faa0b7e558675e10e53822c
Lavanya Mouhamadouvahap <lramapathiran@gmail.com>
2021-06-08 14:15:46 +0200

    ajout des scripts SQL création avec jeu de données

--- ScriptSQL_V2/dataAndDemoLibrary_v2.sql
+++ ScriptSQL_V2/dataAndDemoLibrary_v2.sql
@@ -1,6 +1,6 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
--- Host: localhost    Database: library
+-- Host: 127.0.0.1    Database: library
 -- ------------------------------------------------------
 -- Server version	8.0.19
 
@@ -28,6 +28,7 @@ CREATE TABLE `book` (
   `author` varchar(150) NOT NULL,
   `full_stock` int NOT NULL,
   `remaining_stock` int NOT NULL,
+  `total_pre_booking` int DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -38,7 +39,7 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
-INSERT INTO `book` VALUES (1,'L\'étranger','Albert Camus',10,3),(2,'Bel-Ami','Guy de Maupassant',10,0),(3,'Ne tirez pas sur l\'oiseau moqueur','Harper Lee',10,2),(4,'Sami et Julie, L\'anniversaire de Julie','Emmanuelle Massonaud',10,10),(5,'Arsène Lupin, Gentleman Cambrioleur','Maurice Leblanc',10,10),(6,'On va déguster l\'Italie','François-Régis Gaudry',10,10),(7,'La familia grande','Camille Kouchner',10,10),(8,'L\'Inconnu de la poste','Florence Aubenas',10,9),(9,'Les cinq blessures qui empêchent d\'être soi-même','Lise Bourbeau',10,10),(10,'Si ça saigne','Stephen King',10,10),(11,'Tout le bleu du ciel','Melissa Da Costa',10,10),(12,'Ce n\'était pas de l\'amour','Betty Mannechez, Joëlle Mignot',10,10);
+INSERT INTO `book` VALUES (1,'L\'étranger','Albert Camus',3,0,0),(2,'Bel-Ami','Guy de Maupassant',3,0,1),(3,'Ne tirez pas sur l\'oiseau moqueur','Harper Lee',3,3,0),(4,'Sami et Julie, L\'anniversaire de Julie','Emmanuelle Massonaud',3,1,2),(5,'Arsène Lupin, Gentleman Cambrioleur','Maurice Leblanc',3,3,0),(6,'On va déguster l\'Italie','François-Régis Gaudry',3,3,0),(7,'La familia grande','Camille Kouchner',3,3,0),(8,'L\'Inconnu de la poste','Florence Aubenas',3,0,0),(9,'Les cinq blessures qui empêchent d\'être soi-même','Lise Bourbeau',3,3,0),(10,'Si ça saigne','Stephen King',3,3,0),(11,'Tout le bleu du ciel','Melissa Da Costa',3,3,0),(12,'Ce n\'était pas de l\'amour','Betty Mannechez, Joëlle Mignot',3,0,3);
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +61,7 @@ CREATE TABLE `hibernate_sequence` (
 
 LOCK TABLES `hibernate_sequence` WRITE;
 /*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
-INSERT INTO `hibernate_sequence` VALUES (51),(51),(51);
+INSERT INTO `hibernate_sequence` VALUES (172),(172),(172);
 /*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,7 +84,7 @@ CREATE TABLE `lending` (
   KEY `users_lendings_fk` (`user_id`),
   CONSTRAINT `books_lendings_fk` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
   CONSTRAINT `users_lendings_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,8 +93,67 @@ CREATE TABLE `lending` (
 
 LOCK TABLES `lending` WRITE;
 /*!40000 ALTER TABLE `lending` DISABLE KEYS */;
-INSERT INTO `lending` VALUES (1,'2020-12-11 00:00:00','2021-02-02',1,1,1),(2,'2020-12-11 00:00:00','2021-02-02',1,1,2),(11,'2021-01-08 00:00:00','2021-04-02',1,1,2),(12,'2021-01-08 00:00:00','2021-04-02',1,1,2),(13,'2021-01-08 00:00:00','2021-03-05',1,2,2),(14,'2021-01-13 00:00:00','2021-04-07',1,1,2),(15,'2021-01-14 00:00:00','2012-02-13',1,2,2),(50,'2021-02-15 00:00:00','2021-04-12',1,1,8);
+INSERT INTO `lending` VALUES (1,'2020-12-11 00:00:00','2021-06-04',0,1,1),(12,'2021-01-08 00:00:00','2021-06-26',0,3,2),(13,'2021-01-08 00:00:00','2021-07-01',1,2,2),(50,'2021-02-15 00:00:00','2021-07-24',1,1,8),(51,'2021-05-25 00:00:00','2021-06-20',0,3,4),(52,'2021-05-20 00:00:00','2021-06-17',0,3,12);
 /*!40000 ALTER TABLE `lending` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notified`
+--
+
+DROP TABLE IF EXISTS `notified`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notified` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `notified_date` date NOT NULL,
+  `user_id` int NOT NULL,
+  `prebooking_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_notifieds_fk` (`user_id`),
+  KEY `prebookings_notifieds_fk` (`prebooking_id`),
+  CONSTRAINT `prebookings_notifieds_fk` FOREIGN KEY (`prebooking_id`) REFERENCES `prebooking` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `users_notifieds_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=196 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notified`
+--
+
+LOCK TABLES `notified` WRITE;
+/*!40000 ALTER TABLE `notified` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notified` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prebooking`
+--
+
+DROP TABLE IF EXISTS `prebooking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prebooking` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL,
+  `user_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `books_prebookings_fk` (`book_id`),
+  KEY `users_prebookings_fk` (`user_id`),
+  CONSTRAINT `books_prebookings_fk` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
+  CONSTRAINT `users_prebookings_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=172 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prebooking`
+--
+
+LOCK TABLES `prebooking` WRITE;
+/*!40000 ALTER TABLE `prebooking` DISABLE KEYS */;
+INSERT INTO `prebooking` VALUES (155,'2021-06-04 21:35:15',1,4),(156,'2021-06-04 22:48:08',2,4);
+/*!40000 ALTER TABLE `prebooking` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -139,4 +199,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-16 14:34:21
+-- Dump completed on 2021-06-08 13:53:59
